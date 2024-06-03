ed7ab811340106695b62fd0eaeabe3e3647fa209
Arjun-manikkoth <158160886+Arjun-manikkoth@users.noreply.github.com>
2024-06-03 20:58:14 +0530

    added to .env

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/admin",adminRoute);
 //for user routes
 app.use("/",userRoute);
 
-const PORT = process.env.port||3001
+const PORT = process.env.PORT||3001
 
 app.listen(PORT,()=>{
   console.log("listening to the port")
